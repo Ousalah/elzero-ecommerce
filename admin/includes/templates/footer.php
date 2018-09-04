a84d65e4e9ebe3abd6dab1309b2de01fc451c4e9
Mohamed Ousalah <mohamed@ousalah.com>
2018-09-04 11:31:18 +0100

    adding tools and libraries

--- admin/includes/templates/footer.php
+++ admin/includes/templates/footer.php
@@ -1,5 +1,8 @@
     <div class="footer">
       This is Footer
     </div>
+    <script src="layout/js/jquery-1.12.4.min.js"></script>
+    <script src="layout/js/bootstrap.min.js"></script>
+    <script src="layout/js/backend.js"></script>
   </body>
 </html>
--- admin/includes/templates/header.php
+++ admin/includes/templates/header.php
@@ -3,5 +3,8 @@
   <head>
     <meta charset="utf-8">
     <title>Admin</title>
+    <link rel="stylesheet" href="layout/css/bootstrap.min.css">
+    <link rel="stylesheet" href="layout/css/font-awesome.min.css">
+    <link rel="stylesheet" href="layout/css/backend.css">
   </head>
   <body>
