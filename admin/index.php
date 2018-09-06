1f5890be5dc3d289840648c2703500174d363ad6
Mohamed Ousalah <mohamed@ousalah.com>
2018-09-06 15:49:25 +0100

    code the login form part 1

--- admin/index.php
+++ admin/index.php
@@ -2,9 +2,26 @@
   include "init.php";
   include $tpl . "header.php";
   include $lang . "english.php";
+
+  // Check if User Coming From HTTP Post Request
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['user'];
+    $password = $_POST['pass'];
+    $hashedPass = sha1($password);
+
+    // Check if The User Exist in Database
+    $stmt = $con->prepare("SELECT Username, Password FROM users WHERE username = ? AND password = ? AND GroupID = 1");
+    $stmt->execute(array($username, $hashedPass));
+    $count = $stmt->rowCount();
+
+    // If Count > 0 This Mean The Database Contain Record About This Username
+    if ($count > 0) {
+      echo "Welcom " . $username;
+    }
+  }
 ?>
 
-  <form action="" class="login">
+  <form action="" class="login" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
     <h4 class="text-center">Admin Login</h4>
     <input type="text" class="form-control" name="user" placeholder="Username" autocomplete="off">
     <input type="password" class="form-control" name="pass" placeholder="Password" autocomplete="new-password">
