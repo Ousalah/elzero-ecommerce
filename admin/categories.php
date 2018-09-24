8a3cd0f6bfe519e2ff8b8de1764081a4b446fb72
Mohamed Ousalah <mohamed@ousalah.com>
2018-09-24 17:40:17 +0100

    categories - create insert category page

--- admin/categories.php
+++ admin/categories.php
@@ -114,6 +114,54 @@
 <?php
     } elseif($do == 'insert') { // Start Insert Page
 
+      echo "<h1 class='text-center'>Insert New Category</h1>";
+      echo "<div class='container'>";
+
+      // Check if User Access to These Page by Post Request
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        // Get Variables from the form
+        $category_name            = $_POST["name"];
+        $category_description     = $_POST["description"];
+        $category_ordering        = $_POST["ordering"];
+        $category_visibility      = $_POST["visibility"];
+        $category_allow_comment   = $_POST["allow_comment"];
+        $category_allow_ads       = $_POST["allow_ads"];
+
+        // Validate The form
+        $form_errors = array();
+        if (empty($category_name)) { $form_errors[] = "<div class='alert alert-danger'>Category name can't be <strong>empty</strong>.</div>"; }
+        // Check If Category Name Exist in Database
+        if(checkItem("Name", "categories", $category_name)) { $form_errors[] = "<div class='alert alert-danger'>This category name is already <strong>exists</strong>.</div>"; }
+
+        // Check If There's No Error, Proceed The Insert Operation
+        if (!empty($form_errors)) :
+          // Loop Into Errors Array and Echo It
+          redirectHome($form_errors, "back", (count($form_errors) + 2));
+        else:
+          // Insert Category Info in Database
+          $stmt = $con->prepare("INSERT INTO categories(Name, Description, Ordering, Visibility, Allow_Comment, Allow_Ads)
+          VALUES(:name, :description, :ordering, :visibility, :allow_comment, :allow_ads)");
+          $stmt->execute(array(
+            'name'           => $category_name,
+            'description'    => $category_description,
+            'ordering'       => $category_ordering,
+            'visibility'     => $category_visibility,
+            'allow_comment'  => $category_allow_comment,
+            'allow_ads'      => $category_allow_ads
+          ));
+
+          // Echo Success Message
+          $msg = "<div class='alert alert-success'><strong>" . $stmt->rowCount() . "</strong> Record inserted.</div>";
+          redirectHome($msg, "back");
+        endif;
+
+      } else {
+        $msg = "<div class='alert alert-danger'>Your can not browse to this page <strong>directly</strong>.</div>";
+        redirectHome($msg);
+      }
+      echo "</div>";
+
     } elseif($do == 'edit') { // Start Edit Page
 
     } elseif($do == 'update') { // Start Update Page
