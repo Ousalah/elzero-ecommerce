c7f5f3b20a2f695bd44c2933ba72b1e5cd29bf99
Mohamed Ousalah <mohamed@ousalah.com>
2018-10-08 11:59:52 +0100

    split login and singup forms

--- login.php
+++ login.php
@@ -10,21 +10,29 @@
 
   // Check if User Coming From HTTP Post Request
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $hashedPass = sha1($password);
 
-    // Check if The User Exist in Database
-    $stmt = $con->prepare("SELECT Username, Password FROM users WHERE username = ? AND password = ?");
-    $stmt->execute(array($username, $hashedPass));
-    $count = $stmt->rowCount();
+    if (isset($_POST["login"])):
 
-    // If Count > 0 This Mean The Database Contain Record About This Username
-    if ($count > 0) {
-      $_SESSION['user']     = $username; // Register Session Name
-      header('Location: index.php'); // Redirect To Home Page
-      exit();
-    }
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+      $hashedPass = sha1($password);
+
+      // Check if The User Exist in Database
+      $stmt = $con->prepare("SELECT Username, Password FROM users WHERE username = ? AND password = ?");
+      $stmt->execute(array($username, $hashedPass));
+      $count = $stmt->rowCount();
+
+      // If Count > 0 This Mean The Database Contain Record About This Username
+      if ($count > 0):
+        $_SESSION['user']     = $username; // Register Session Name
+        header('Location: index.php'); // Redirect To Home Page
+        exit();
+      endif;
+
+    else:
+      header('Location: dashboard.php');
+    endif;
+
   }
 ?>
 
@@ -41,12 +49,12 @@
     <div class="input-container">
       <input type="password" class="form-control" name="password" placeholder="Password" required autocomplete="new-password">
     </div>
-    <input type="submit" class="btn btn-primary btn-block" value="Login">
+    <input type="submit" class="btn btn-primary btn-block" name="login" value="Login">
   </form>
   <!-- End login form -->
 
   <!-- Start singup form -->
-  <form class="singup">
+  <form class="singup" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
     <div class="input-container">
       <input type="text" class="form-control" name="username" placeholder="Username" required autocomplete="off">
     </div>
@@ -59,9 +67,15 @@
     <div class="input-container">
       <input type="email" class="form-control" name="email" required placeholder="example@domain.com">
     </div>
-    <input type="submit" class="btn btn-success btn-block" value="Singup">
+    <input type="submit" class="btn btn-success btn-block" name="singup" value="Singup">
   </form>
   <!-- End singup form -->
+
+  <!-- Start -->
+  <div class="the-errors">
+
+  </div>
+  <!-- End -->
 </div>
 
 <?php include $tpl . "footer.php"; ?>
