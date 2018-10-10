7c3912cb14ede69a0bc49b586d3db6135fdb861b
Mohamed Ousalah <mohamed@ousalah.com>
2018-10-10 16:06:10 +0100

    show comments box - part 1

--- items.php
+++ items.php
@@ -19,6 +19,7 @@
 ?>
     <h1 class="text-center"><?php echo $item["Name"] ?></h1>
     <div class="container">
+      <!-- Start item details -->
       <div class="row">
         <div class="col-md-3">
           <img class="img-responsive img-thumbnail center-block" src="https://via.placeholder.com/350x200" alt="">
@@ -50,15 +51,43 @@
           </ul>
         </div>
       </div>
+      <!-- End item details -->
       <hr class="custom-hr">
+      <!-- Start add comment form -->
+      <?php if (isset($_SESSION['user'])): ?>
+        <div class="row">
+          <div class="col-md-offset-3">
+            <div class="add-comment">
+              <h3>Add Your Comment</h3>
+              <form action="<?php echo $_SERVER["PHP_SELF"] . "?itemid=" . $itemid ?>" method="post">
+                <textarea class="form-control" name="comment" cols="30" rows="10"></textarea>
+                <input type="submit" class="btn btn-primary" value="Add Comment">
+              </form>
+              <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                  echo "comment : " . $_POST["comment"] . "<br />";
+                  echo "item : " . $itemid . "<br />";
+                  echo "user : " . $_SESSION["userid"];
+                }
+              ?>
+            </div>
+          </div>
+        </div>
+      <?php else: ?>
+        <div class="lead"><a href="login.php">Login or Register</a> to add comment.</div>
+      <?php endif; ?>
+      <!-- End add comment form -->
+      <hr class="custom-hr">
+      <!-- Start comments list -->
       <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-2">
           <img class="img-responsive img-thumbnail center-block" src="https://via.placeholder.com/350x200" alt="">
         </div>
-        <div class="col-md-9">
+        <div class="col-md-10">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
       </div>
+      <!-- End comments list -->
     </div>
 <?php
   else:
